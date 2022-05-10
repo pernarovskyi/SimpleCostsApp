d9c4d054865a11360685d6308296e899f6466c08
ypernarovskyi@softjourn.com <ypernarovskyi@softjourn.com>
2022-05-10 10:39:03 +0300

    added initial files and gitignore

---
+++ SimpleCostsApplication/Context/CostsContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCostsApplication.Models;
+
+namespace SimpleCostsApplication.Context
+{
+    public class CostsContext : DbContext
+    {
+      public DbSet<Costs> Costs { get; set; }
+
+        public CostsContext(DbContextOptions<CostsContext> options) : base(options) {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Costs>().ToTable("Costs");            
+        }
+    }
+
+ 
+}
