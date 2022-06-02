2851db0248e615cfb71bd8b0ce3e73005a07af7b
Yurii Pernarovskyi <j.pernarovskyi@gmail.com>
2022-06-02 21:06:25 +0300

    Repository and Models to work with Users.

---
+++ CostApplication/DTO/UserDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostApplication.DTO
+{
+    public class UserDto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+        public DateTime? LastVisitedDate { get; set; }
+    }
+}
--- CostApplication/Data/IAppDBContext.cs
+++ CostApplication/Data/IAppDBContext.cs
@@ -1,11 +1,13 @@
 ﻿using CostApplication.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace CostApplication.Data
 {
     public interface IAppDBContext
     {
         DbSet<Cost> Costs { get; set; }
+        DbSet<User> Users { get; set; }
 
         int SaveChanges();
     }
